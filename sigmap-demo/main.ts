255fa6554e5115a2613ce877cea81b3c94d94b26
williamhubaux <william.hubaux@gmail.com>
2025-08-19 22:13:09 +0200

    Update : - Minor change on the demo project

--- sigmap-demo/main.ts
+++ sigmap-demo/main.ts
@@ -7,14 +7,8 @@ async function demo() {
     const sigmap = new Sigmap({ canvas: gpuCanvas });
     await sigmap.init();
 
-    await sigmap.loadMapShaders("/shaders/heatmap.wgsl");
-
-    const csvText = await fetch("/data/cities.csv").then(r => r.text());
-    const rows = await sigmap.loadCSV(csvText);
-    console.log(rows);
-
     const basemapOptions = {
-        zoom: 3,
+        zoom: 2,
         center: { lat: 0, lon: 0 },
     }
 
